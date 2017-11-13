af7884391ea27d5d3d34cb2c6f246ec5196aa76c
houbingbing <52445295@qq.com>
2017-11-13 14:08:02 +0800

    textarea 导出数据

--- app/scripts/directives/export/list.js
+++ app/scripts/directives/export/list.js
@@ -18,6 +18,7 @@ define([
                             template: '<div modal-panel title="title" tmpl="tmpl"></div>',
                             controller: function ($scope, $uibModalInstance) {
                                 $scope.param = $scope.param || {};
+                                $scope._param = $scope._param || {};
                                 $scope.title = supscope.data.desc || '导出数据';
                                 $scope.tmpl = '<form class="form-horizontal" name="FormBody" novalidate>' +
                                     '<style type="text/css"> .modal .checkbox-inline{margin-left: 0;}' +
@@ -37,6 +38,8 @@ define([
                                         $scope.tmpl += '<div form-date-time="" text="' + val.name + '" ng-model="param.' + key + '"  ' + (val.required ? ('required=\"true\"') : '') + '></div>';
                                     } else if (val.type == 'text') {
                                         $scope.tmpl += '<div form-input text="' + val.name + '" ng-model="param.' + key + '"  ' + (val.required ? ('required=\"true\"') : '') + '></div>';
+                                    } else if (val.type == 'textarea') {
+                                        $scope.tmpl += '<div form-textarea text="' + val.name + '" ng-model="_param.' + key + '"  ' + (val.required ? ('required=\"true\"') : '') + ' placeholder="多条数据回车换行,否则会出错" rows="10"></div>';
                                     }
                                     if (val.default_val) {
                                         eval('$scope.param.' + key + ' = ' + val.default_val);
@@ -45,6 +48,12 @@ define([
                                 $scope.tmpl += '<a class="btn btn-primary btn-rounded pull-right" ng-disabled="FormBody.$invalid"  ng-click="exec();">执行</a>'
                                 $scope.tmpl += '</form>';
                                 $scope.exec = function () {
+                                    angular.forEach($scope.condition, function (val, key) {
+                                        if (val.type == 'textarea') {
+                                            $scope.param[key] = $scope._param[key].replace(/\n/g, ',').split(',');
+                                            // console.log($scope.param[key]);
+                                        }
+                                    });
                                     angular.extend($scope.param, {command: $scope.command});
                                     if (confirm('确认将查询结果导出EXCEL吗?')) {
                                         widget.ajaxRequest({
