c3b84fd755739d3f37e270bcd3e4994ca7df6f98
houbingbing <52445295@qq.com>
2017-11-29 16:04:02 +0800

    旋转图片

--- app/scripts/directives/show_image.js
+++ app/scripts/directives/show_image.js
@@ -21,17 +21,37 @@ define([
                     // console.log($scope.url.substring($scope.url.length-4,$scope.url.length));
                     $scope.widthPx = ($scope.width && $scope.width.indexOf('%') > -1) ? ($scope.width) : ($scope.width || 200) + 'px';
                     // $scope.type = supscope.url.substring(supscope.url.length - 4, supscope.url.length);
-                    $scope.rotateObj = {};
+                    $scope.imgObj = {};
+                    $scope.rotateGroupBtn = [];
                     //顺时针 度数
-                    $scope.rotateImg = function (rotate) {
+                    var oImg = new Image();
+                    $scope.rotateImg = function (rotate, rotateGroupBtnIndex) {
+                        if (rotateGroupBtnIndex != undefined) {
+                            $scope.rotateGroupBtn = [0, 0, 0, 0];
+                            $scope.rotateGroupBtn[rotateGroupBtnIndex] = 1;
+                        }
+                        if (!oImg.src) {
+                            $scope.url = 'http://resource.huijiame.com/media_f1f266a2fb57bbad95ea8ec6e70e4af4';
+                            oImg.src = $scope.url;
+                            oImg.onload = function () {
+                                $scope.imgObj.clientWidth = $($element).find('.imgObjClass').width();
+                                $scope.imgObj.clientHeight = $($element).find('.imgObjClass').height();
+                            }
+                        } else {
+                        }
+
+                        // $($element).find('img')[0].width(), $($element).find('img')[0].height()
+                        // console.log($($element));
                         if (rotate != undefined && angular.isNumber(rotate)) {
+                            // console.log($($element).height());
                             $scope.rotateObj = {
+                                "margin": "0 auto",
                                 "transform": "rotate(" + rotate + "deg)",
-                                "max-height": $($element).width(),
-                                "margin": "auto"
-                            };
-                        };
-                    };
+                                "max-height": (rotate % 180 != 0) ? (($scope.imgObj.clientHeight < $scope.imgObj.clientWidth) ? $scope.imgObj.clientWidth : $scope.imgObj.clientWidth) : '1',
+                                "max-width": (rotate % 180 != 0) ? (($scope.imgObj.clientHeight < $scope.imgObj.clientWidth) ? $scope.imgObj.clientWidth : $scope.imgObj.clientWidth) : '1'
+                            }
+                        }
+                    }
                     if ($scope.rotate) {
                         $scope.rotateImg(0);
                     }
