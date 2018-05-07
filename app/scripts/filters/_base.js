c33b08494a465aa63ea69bae4c6943cba3ccd489
houbingbing <houbingbing@ahaschool.com>
2018-05-07 19:05:18 +0800

    小程序过滤查询

--- app/scripts/filters/_base.js
+++ app/scripts/filters/_base.js
@@ -135,6 +135,9 @@ define([
                     case '6':
                         rtn = '支付宝H5';
                         break;
+                    case '9':
+                        rtn = '小程序支付';
+                        break;
                     default :
                         rtn = '未选择';
                 }
