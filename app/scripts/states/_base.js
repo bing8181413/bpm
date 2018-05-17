1d198c2f7721e382ebcdf2cbf608d56b9bbd17dc
houbingbing <houbingbing@ahaschool.com>
2018-05-17 17:56:50 +0800

    适应webpack

--- app/scripts/states/_base.js
+++ app/scripts/states/_base.js
@@ -1,78 +1,68 @@
 define([
-    'states/states',
-    'states/hjm/container',// 总体的
-    'states/hjm/groupbuy',// 拼团
-    'states/hjm/account',// 后台账户
-    'states/hjm/product',// 商品
-    'states/hjm/order',// 订单
-    'states/hjm/banner',// 运营位
-    'states/hjm/export',// 导出
-    'states/hjm/user',// 用户
-    'states/hjm/refund',// 财务退款
-    'states/hjm/delivery',// 配送
-    'states/hjm/coupon',//优惠券
-    'states/hjm/exchangecode',//兑换码
-    'states/hjm/community',//小区
-    'states/hjm/sms',//发送短信
-    'states/hjm/supports',//支持城市
-    'states/hjm/web',//网站推广
-    'states/hjm/subject',//运营专题
-    'states/hjm/resource',//运营资源库
-    'states/hjm/orders',//维护订单数据
-    'states/hjm/wechat',//微信相关数据
-    'states/hjm/hotkey',//热搜词
-    'states/hjm/captcha',//验证码
-    'states/hjm/survey_category',//测评维度
-    'states/hjm/survey_question',//测评题库
-    'states/hjm/survey_question_attachment',//测评附加问题题库
-    'states/hjm/survey_plan',//测评管理
-    'states/hjm/survey_list',//测评入口
-    'states/hjm/lessons',//课程
-    'states/hjm/mission',//课程任务
-    'states/hjm/teacher',//老师
-    'states/hjm/student',//学生
-    'states/hjm/work',//作业管理
-    'states/hjm/redpacketcoupons',//购买成功后的 分享红包
-    'states/hjm/live_rooms',//直播房间
-    'states/hjm/record_rooms',//点播房间
+  './states',
+  './hjm/container',// 总体的
+  './hjm/groupbuy',// 拼团
+  './hjm/account',// 后台账户
+  './hjm/product',// 商品
+  './hjm/order',// 订单
+  './hjm/banner',// 运营位
+  './hjm/export',// 导出
+  './hjm/user',// 用户
+  './hjm/refund',// 财务退款
+  './hjm/delivery',// 配送
+  './hjm/coupon',//优惠券
+  './hjm/exchangecode',//兑换码
+  './hjm/community',//小区
+  './hjm/sms',//发送短信
+  './hjm/supports',//支持城市
+  './hjm/web',//网站推广
+  './hjm/subject',//运营专题
+  './hjm/resource',//运营资源库
+  './hjm/orders',//维护订单数据
+  './hjm/wechat',//微信相关数据
+  './hjm/hotkey',//热搜词
+  './hjm/captcha',//验证码
+  './hjm/survey_category',//测评维度
+  './hjm/survey_question',//测评题库
+  './hjm/survey_question_attachment',//测评附加问题题库
+  './hjm/survey_plan',//测评管理
+  './hjm/survey_list',//测评入口
+  './hjm/lessons',//课程
+  './hjm/mission',//课程任务
+  './hjm/teacher',//老师
+  './hjm/student',//学生
+  './hjm/work',//作业管理
+  './hjm/redpacketcoupons',//购买成功后的 分享红包
+  './hjm/live_rooms',//直播房间
+  './hjm/record_rooms',//点播房间
 
-    'states/hjm/cert',//问答证书
+  './hjm/cert',//问答证书
 
+  './hjm/cctalk_account',//CCtalk 账户列表
+  './hjm/zhibo_qr_code',//CCtalk 直播二维码
 
-    'states/hjm/cctalk_account',//CCtalk 账户列表
-    'states/hjm/zhibo_qr_code',//CCtalk 直播二维码
+  './hjm/market/combinationcoupons',//组合优惠券
+  './hjm/market/activitycoupons',//优惠券
+  './hjm/market/exchanges',//生成兑换码
 
-    'states/hjm/market/combinationcoupons',//组合优惠券
-    'states/hjm/market/activitycoupons',//优惠券
-    'states/hjm/market/exchanges',//生成兑换码
+  './hjm/market/market',//market 总体的
 
-    'states/hjm/market/market',//market 总体的
+  './hjm/live/live',//live 总体的
 
-    'states/hjm/live/live',//live 总体的
+  './hjm/batchproduct/batchproducts',//批量活动管理
 
+  './hjm/dashboard/dashboard',//活动面板  报表
 
+  './hjm/merchant/merchant',//供应商管理
 
-    'states/hjm/batchproduct/batchproducts',//批量活动管理
+  './hjm/support/sku',//sku
 
+  './hjm/sqls',// exports/sqls 导出sql语句
 
-    'states/hjm/dashboard/dashboard',//活动面板  报表
+  './demo/demo',//demo index
+  './demo/baidumap',//百度地图
+  './demo/datetime',//时间 日期插件
 
-
-
-    'states/hjm/merchant/merchant',//供应商管理
-
-
-    'states/hjm/support/sku',//sku
-
-    'states/hjm/sqls',// exports/sqls 导出sql语句
-
-
-    'states/demo/demo',//demo index
-    'states/demo/baidumap',//百度地图
-    'states/demo/datetime',//时间 日期插件
-
-
-
-], function () {
+], function() {
 
 });
