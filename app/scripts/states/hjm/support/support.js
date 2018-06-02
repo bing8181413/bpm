fca3acb1a44c624491d23685a8c6e944c284d9d4
houbingbing <houbingbing@ahaschool.com>
2018-06-02 17:20:35 +0800

    define 加上一个function

--- app/scripts/states/hjm/support/support.js
+++ app/scripts/states/hjm/support/support.js
@@ -3,7 +3,9 @@
  * 默认的产品首页和默认的跳转规则
  */
 define([
-  '../../states'
-  , '../../../cons/simpleCons'
-  , './paymethods',//app 支付方式 配置
-]);
+      '../../states'
+      , '../../../cons/simpleCons'
+      , './paymethods',//app 支付方式 配置
+    ],
+    function(stateModule, cons) {
+    });
