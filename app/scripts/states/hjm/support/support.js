4390e14d1c2b492656926c10ac8df10b2dafd93c
houbingbing <houbingbing@ahaschool.com>
2018-07-30 11:01:35 +0800

    标点符号修改

--- app/scripts/states/hjm/support/support.js
+++ app/scripts/states/hjm/support/support.js
@@ -3,10 +3,10 @@
  * 默认的产品首页和默认的跳转规则
  */
 define([
-      '../../states'
-      , '../../../cons/simpleCons'
-      , './paymethods',//app 支付方式 配置
-      , './iosAuditversion',//IOS审核  版本更新
+        '../../states'
+        , '../../../cons/simpleCons'
+        , './paymethods'               //app 支付方式 配置
+        , './iosAuditversion',          //IOS审核  版本更新
     ],
     function(stateModule, cons) {
     });
